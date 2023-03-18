7715ef79111e51736161ff478b6ada9f86285e03
Carlos Gallego <gallegocarlosmario1979@gmail.com>
2023-03-18 12:06:36 -0500

    creacion de servidor

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+const HOST = "localhost";
+const PORT = 8000;
+
+const writeHTMLresponse = (res,htmlCode) =>{
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(200);
+    res.end(htmlCode);
+}
+
+const server= http.createServer((req, res) => {
+ const url = req.url;
+ console.log("URL es ", url);
+ if(url== "/other"){
+    writeHTMLresponse(res, "<p> Esta es otra ruta</p>");
+ } else {
+    writeHTMLresponse(res, "<p> codigo HTML </P>"); 
+ }
+})
+
+server.listen(PORT, HOST, () =>{
+
+console.log(`servidor corriendo en http://${HOST}:${PORT}`);
+
+}
+
+)
